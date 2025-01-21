68b3fcb6a2c9242d6ffea579ca0aed83190406b0
ZarghamAli-65 <zarghamworks@gmail.com>
2025-01-21 11:41:43 +0500

    auth

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,5 +1,17 @@
+import { prisma } from "@/utils/connect";
 import { NextResponse } from "next/server";
 
-export const GET = () => {
-    return new NextResponse("Hello", { status: 200 });
+
+// fetch all categories
+export const GET = async () => {
+    try {
+
+        const categories = await prisma.category.findMany()
+        return new NextResponse(JSON.stringify(categories), { status: 200 })
+
+    }
+    catch (err) {
+        console.log(err)
+        return new NextResponse(JSON.stringify({ message: "Some thing went wrong!!!" }), { status: 500 })
+    }
 }
